4c3093f48679470b0095580765665d5a572958ae
Sadat Ahmad <s.ahmad-1@student.utwente.nl>
2022-07-05 00:15:55 +0530

    Step 1 Done

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import { ApolloServer, gql } from 'apollo-server';
+const { ApolloServer, gql } = require('apollo-server');
+var admin = require("firebase-admin");
 
-const schema = gql`
-    type Mutation {
-        #mutationt to create/update a user with ID, name and IFSC as input
-        addAccountDetails(id: ID!, name: String!, accounts: [ifsc]!): User
-    }
-    
+var serviceAccount = require("./solvee-intern-8b67252d5b2f.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true })
+
+const typeDefs = gql`
+  
     type User {
-        id: ID!
-        name: String!
+        id: ID
+        name: String
         accounts: [Account]
     }
 
@@ -28,6 +34,60 @@ const schema = gql`
     type Weather {
         temp: Int
         humidity: Int
-    }`
-
+    }
     
+    type Mutation {
+        #mutationt to create/update a user with ID, name and IFSC as input
+        addAccountDetails(userID: ID!, userName: String!, accounts: [String]!): User
+    }
+
+    type Query {
+        users: [User]
+    }
+    `;
+
+const resolvers = {
+    Query: {
+        async users() {
+            const accounts = await db.collection('User').get();
+            return accounts.docs.map(account => account.data());
+        },
+    },
+    User: {},
+    Account: {},
+    Weather: {},
+    Mutation: {
+        addAccountDetails: async (_, {userID, userName, accounts}) => {
+            //Adding or Updating User on DB
+            let user = await db.collection('User').where('id', '==', userID).get();
+            if (!user.empty) { 
+                console.log(user.docs[0].data())
+                await db.collection('User').doc(user.docs[0].id).update({
+                    id: userID,
+                    name: userName
+                })
+            }
+            else {
+                db.collection('User').doc(userID).set({
+                    id: userID,
+                    name: userName
+                })
+            }
+            user = await db.collection('User').where('id', '==', userID).get();
+            res = user.docs[0].data(); 
+            //Add bank data from IFSC endpoint
+            
+            console.log(res);
+            return res;
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
